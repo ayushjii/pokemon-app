eb0574234b7bea5dc1ea675bb61bd3d6dabd2103
ayushjii <pawangoyal6789@gmail.com>
2022-08-28 04:57:21 +0530

    b

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,7 +1,1 @@
-import axios from 'axios'
-
-const api = axios.create({
-    baseURL: 'https://pokeapi.co/api/v2/pokemon?limit=15/'
-})
-
-export default api
+export const API = "https://pokeapi.co/api/v2/pokemon/?limit=25" ; 
--- src/Components/AllPokemon.js
+++ src/Components/AllPokemon.js
@@ -1,53 +1,55 @@
-import React, { useEffect, useState } from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
+import axios from "axios";
+import React, { Component } from "react";
+import InfiniteScroll from "react-infinite-scroller";
 import { Link } from "react-router-dom";
-import api from "../API/Api";
-import api_details from "../API/Api_details";
+import { API } from "../API/api";
 import Mypokemon from "./Mypokemon";
-import Spinner from './Spinner.js';
+import Spinner from "./Spinner.js";
 
 
-function All() {
-  const [allPokemons, setAllPokemons] = useState([]);
-  const [list, setList] = useState([]);
+export default class All extends Component {
+  state = {
+    url: API,
+    pokemon: [],
+    itemsCountPerPage: 20,
+    activePage: 1,
+  };
 
-useEffect(() => {
-    api_details.get().then(({ data }) => {
-      setList(data);
+  loadPokemon = () => {
+    axios.get(this.state.url).then((res) => {
+      this.setState((prevState) => {
+        return {
+          pokemon: [...prevState.pokemon, ...res.data.results],
+          url: res.data.next,
+        };
+      });
     });
-    // eslint-disable-next-line
-  }, [])
+  };
 
-  useEffect(() => {
-    api.get().then(({ data }) => {
-
-      setAllPokemons(data.results);
-      console.log(data.results);
-    });
-  }, []);
-
-  return (
-    <div>
-
-    <Link to='/test'> crollllllllll;fbnkgjksgjkgkjejkgsjk jkjkjk jkssgj</Link>
-      <InfiniteScroll
-        dataLength={allPokemons.length}
-        next={() => setList}
-        hasMore={true}
-        loader={<Spinner />}
-      >
-      {list.name}
-        <div className="">
-          {allPokemons.map((pokemon) => (
-             <button key={pokemon.name}>
-              <Link to={"/pokemon/" + pokemon.name}>
-                <Mypokemon name={pokemon.name} />
-              </Link>
-            </button>
-          ))}
-        </div>
-      </InfiniteScroll>
-    </div>
-  );
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.pokemon ? (
+          <div className="row">
+            <InfiniteScroll
+              pageStart={0}
+              loadMore={this.loadPokemon}
+              hasMore={this.state.url}
+              loader={<Spinner />}
+            >
+              {this.state.pokemon.map((pokemon, i) => (
+                <button key={i}>
+                  <Link to={"/pokemon/" + pokemon.name}>
+                    <Mypokemon name={pokemon.name} />
+                  </Link>
+                </button>
+              ))}
+            </InfiniteScroll>
+          </div>
+        ) : (
+          <Spinner />
+        )}
+      </React.Fragment>
+    );
+  }
 }
-export default All;
