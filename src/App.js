8dd576f5e9a2e77281a8cddd7413c98c21e8d41a
ayushjii <pawangoyal6789@gmail.com>
2022-08-30 14:01:44 +0530

    p

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+import Favorite from "./Components/Favorites";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Aboutpokemon from "./Components/Aboutpokemon";
 import { Home } from "./Components/Home";
+import War from "./Components/War";
 
 const App = () => {
   return (
     <Router>
-        <Routes>
-          <Route path="/pokemon/" element={<Aboutpokemon />} />
-          <Route path="/pokemon/:name" element={<Aboutpokemon />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
+      <Routes>
+        <Route path="/pokemon/" element={<Aboutpokemon />} />
+        <Route path="/favorites" element={<Favorite />} />
+        <Route path="/war" element={<War />} />
+        <Route path="/pokemon/:name" element={<Aboutpokemon />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
--- src/Components/Mypokemon.jsx
+++ src/Components/Mypokemon.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import api_details from "../API/Api_details";
+import {GiAxeSword} from 'react-icons/gi';
+import {BsFillHeartFill} from 'react-icons/bs';
+import {BiMessageSquareDetail} from 'react-icons/bi'
 
 const Mypokemon = ({ name }) => {
   const [list, setList] = useState([]);
@@ -27,6 +31,11 @@ const Mypokemon = ({ name }) => {
         alt={list.name}
       />
       <h3>{list?.types?.map((item) => item.type.name.concat(" "))}</h3>
+      <div className="mt-0.5">
+        <Link to={"/pokemon/" + list.name}><button className="bg-white p-1.5 rounded-md shadow hover:shadow-69xl mx-2"><BiMessageSquareDetail size={25}/></button></Link>
+        <Link to={"/favorites"}><button className="bg-white p-1.5 rounded-md shadow hover:shadow-69xl mx-2"><BsFillHeartFill size={25}/></button></Link>
+        <Link to={"/war"}><button className="bg-white p-1.5 rounded-md shadow hover:shadow-69xl mx-2"><GiAxeSword size={25}/></button></Link>
+      </div>
     </div>
   );
 };
