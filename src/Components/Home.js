c723487466a7c0448e588e902e6e1fd6494268c4
ayushjii <pawangoyal6789@gmail.com>
2022-08-30 05:59:36 +0530

    top-scroll

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import All from './AllPokemon'
-import { Searchbar } from './Searchbar'
+import React from "react";
+import ScrollToTop from "react-scroll-to-top";
+import All from "./AllPokemon";
+import { Searchbar } from "./Searchbar";
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
 export const Home = () => {
   return (
-    <div className='hel'>
-        <Searchbar />
-        <All/>
+    <div>
+      <ScrollToTop
+        smooth
+        component={
+          <div className="ml-1.5">
+            <BsFillArrowUpCircleFill color="red" size={25} />
+          </div>
+        }
+      />
+      <Searchbar />
+      <All />
     </div>
-  )
-}
+  );
+};
