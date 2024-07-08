ecf413faa9458c3f31661c73a0c9d7f43a380bd1
ayushjii <pawangoyal6789@gmail.com>
2024-07-09 01:47:09 +0530

    comp

--- src/API/Apilimit.js
+++ src/API/Apilimit.js
@@ -1,7 +1,3 @@
-import axios from 'axios'
-
-const API_Limit = axios.create({
-    baseURL: 'https://pokeapi.co/api/v2/pokemon?limit=25/'
-})
+const API_Limit = "https://pokeapi.co/api/v2/pokemon/?limit=25" ; 
 
 export default API_Limit
